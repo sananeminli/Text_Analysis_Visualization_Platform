ae79e81d851aef43366c400c072aaad38caf8b2d
sananeminli <senan97@gmail.com>
2023-12-15 20:50:11 +0300

    Adding SNA results to the page. Adding to get comments based on the video and desired number#7 #6

--- streamlit.py
+++ streamlit.py
@@ -6,7 +6,6 @@ import pandas as pd
 from textblob import TextBlob 
 from langdetect import detect  
 import matplotlib.pyplot as plt
-import re
 from wordcloud import WordCloud
 from dotenv import load_dotenv
 import os
@@ -14,8 +13,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 import networkx as nx
-import plotly.graph_objects as go
-from pyvis.network import Network
 import mpld3
 import streamlit.components.v1 as components
 
@@ -33,28 +30,25 @@ youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=youtube_
 
 
 
-def search_youtube(query, max_results=10):
+def search_youtube(query, max_results=30):
+    
     try:
        
         search_response = youtube.search().list(
             q=query,
             type="video",
-            part="id",
+            part="id,snippet",
             maxResults=max_results,
             eventType="completed"
         ).execute()
-
-        # Extract video IDs from the search results
-        video_ids = [item["id"]["videoId"] for item in search_response.get("items", [])]
-
-        return video_ids
+        st.session_state.videos = search_response.get("items", [])
 
     except googleapiclient.errors.HttpError as e:
         print(f"YouTube API error occurred: {e}")
         return []
 
 
-def get_video_comments(video_id, max_results=100):
+def get_video_comments(video_id, max_results):
     try:
         # Call the commentThreads.list method to retrieve comments for a video
         comment_response = youtube.commentThreads().list(
@@ -149,13 +143,33 @@ def generate_network_graph(G , pos):
     nx.draw(G,pos, with_labels=True,node_color='white',  font_size=10, bbox=dict(boxstyle="round,pad=0.3", alpha=0.5))
     # Use mpld3 to save the figure as HTML
     html_content = mpld3.fig_to_html(fig)
-    mpld3.show(fig=fig, ip='127.0.0.1', port=8888, n_retries=50, local=True, open_browser=True, http_server=None)  
+    #mpld3.show(fig=fig, ip='127.0.0.1', port=8888, n_retries=50, local=True, open_browser=True, http_server=None)  
     return html_content
 
+def sna(G):
+    degree_centrality = nx.degree_centrality(G)
+    closeness_centrality = nx.closeness_centrality(G)
+    betweenness_centrality = nx.betweenness_centrality(G)
+    eigenvector_centrality = nx.eigenvector_centrality(G)
+    centralities_dict = {
+    'Node': list(G.nodes()),
+    'Degree Centrality': list(degree_centrality.values()),
+    'Closeness Centrality': list(closeness_centrality.values()),
+    'Betweenness Centrality': list(betweenness_centrality.values()),
+    'Eigenvector Centrality': list(eigenvector_centrality.values())}
+    return  pd.DataFrame(centralities_dict)
+
+
+
+
+
+
+     
 
 st.title("Text Analysis App")
 query = st.text_input("Enter YouTube search query:")
-button_pressed = st.button("Start Analysis")
+
+search = st.button("Seacrh videos!")
 
 if 'wordcloud_image' not in st.session_state:
         # If not, initialize it
@@ -175,22 +189,73 @@ if 'graph_pos' not in st.session_state:
 if 'graph_neg' not in st.session_state:
         # If not, initialize it
         st.session_state.graph_neg = None
+if 'graph_neu' not in st.session_state:
+        # If not, initialize it
+        st.session_state.graph_neu = None
 if 'pos' not in st.session_state:
         # If not, create an empty placeholder
-        st.session_state.pos_pos, st.session_state.neg_pos = None, None
+        st.session_state.pos_pos, st.session_state.neg_pos   ,  st.session_state.neu_pos = None, None, None
+if 'videos' not in st.session_state:    
+    st.session_state.videos = []
+if 'id' not in st.session_state:
+    st.session_state.id = []
+
+
+
+
+with st.spinner("Search... :mag:"):
+    if search:
+        if len(st.session_state.id)>0:
+            st.session_state.id = []
+        search_youtube(query)
+
+
+
+if len(st.session_state.videos)>0:
+     
+    for video in st.session_state.videos:
+         video_id = video['id']['videoId']
+         video_title = video['snippet']['title']
+         video_description = video['snippet']['description']
+         # Display video information and checkbox
+         checkbox_key = f"checkbox_{video_id}"
+         
+         # Check the current state of the checkbox
+         is_checked = st.checkbox(f"{video_title}\n{video_description}", key=checkbox_key)
+         
+         # Check if the checkbox was previously checked but is now unchecked
+         was_checked = video_id in st.session_state.id
+         if was_checked and not is_checked:
+             # Remove video ID from the array
+             st.session_state.id.remove(video_id)
+         elif is_checked and not was_checked:
+             # If checkbox is checked, add video ID to the array
+             st.session_state.id.append(video_id)
+
+
+        # Display selected video IDs
+    st.write('Selected Video IDs:', st.session_state.id)
+
+
+
+
+
+
+
+
 
 def analyze():
     if len(st.session_state.wordcloud_image)>0:
         for path in st.session_state.wordcloud_image:
             os.remove(path)
-    video_ids = search_youtube(query)
+    video_ids = st.session_state.id
     wordcloud_images = []
     all_comments_data = {'Comment': [], 'Polarity': [], 'Size': []}
     update = st.empty()
     update.text("💭 Getting comments from videos...") 
     # Get comments for each of the first four videos
     for video_id in video_ids:
-        comments = get_video_comments(video_id)
+        comments = get_video_comments(video_id , max_commen_result)
         for comment in comments:
             polarity = analyze_sentiment(comment)
             # Detect the language of the comment
@@ -215,11 +280,14 @@ def analyze():
     # Count the number of positive and negative comments
     positive_comments = comments_df[comments_df['Polarity'] > 0]
     negative_comments = comments_df[comments_df['Polarity'] < 0]
+    neutural_comments = comments_df[comments_df['Polarity'] == 0]
     step_size = 10
     array_of_negative_parts = []
     array_of_positive_parts = []
+    array_of_neutural_parts = []
+
     update.text("📚 Doing sentiment analysis...")
-    # Loop through the Negative Comments DataFrame in steps of 10
+# Loop through the Negative Comments DataFrame in steps of 10
 # Loop through the Negative Comments DataFrame in steps of 10
     for i in range(0, len(negative_comments), step_size):
     
@@ -231,7 +299,7 @@ def analyze():
         #son_str  = re.sub(r'[^a-zA-Z0-9\s.,!?]', '', son)
         # Append the result to the list
 
-    array_of_negative_parts.append(concatenated_string)
+        array_of_negative_parts.append(concatenated_string)
     update.text("👍 Done")
     # Loop through the Positive Comments DataFrame in steps of 10
     for i in range(0, len(positive_comments), step_size):
@@ -245,6 +313,19 @@ def analyze():
 
         # Append the result to the list
         array_of_positive_parts.append(concatenated_string)
+    
+    
+    for i in range(0, len(neutural_comments), step_size):
+    
+        df_part = neutural_comments.iloc[i:i + step_size]
+
+        concatenated_string = '.'.join(df_part['Comment'])
+
+        #son = concatenated_string.replace('\n', '')
+        #son_str  = re.sub(r'[^a-zA-Z0-9\s.,!?]', '', son)
+
+        # Append the result to the list
+        array_of_neutural_parts.append(concatenated_string)
 
 
     
@@ -252,6 +333,8 @@ def analyze():
     negative_pos = []
     positive_entities = []
     positive_pos = []
+    neutural_entities = []
+    neutural_pos = []
 
     update.text("🔗 Getting entity links...")
     for i in range(0, len(array_of_negative_parts)): 
@@ -264,6 +347,10 @@ def analyze():
         positive_entities.append(ent)
         positive_pos.append(pos)
     
+    for i in range(0, len(array_of_neutural_parts)): 
+        ent,  pos = tagme_entity_linking(array_of_neutural_parts[i])
+        neutural_entities.append(ent)
+        neutural_pos.append(pos)
     
     G = nx.Graph()
     df_comment_index = 0
@@ -348,8 +435,44 @@ def analyze():
     #plt.savefig("graph_pos.png", format="png", dpi=300)
     #st.session_state.graph_pos = "graph_pos.png"
 
+    G = nx.Graph()
+    df_comment_index = 0
+    last_comment = 0
+    df_size = len(neutural_comments)
+    for i in range(len(neutural_pos)):
+        for j in range(len(neutural_pos[i])):
+            if len(neutural_pos[i])==0:
+                continue
+            indexxx = 0
+            for x in range(df_comment_index , df_comment_index+5):
+                if x==df_size:
+                    break
+                
+                if indexxx < neutural_pos[i][j] < indexxx+int(neutural_comments.iloc[x, neutural_comments.columns.get_loc("Size")]):
+                    print(neutural_entities[i][j]) #entitiy
+                    #print(negative_comments.iloc[x, negative_comments.columns.get_loc("Comment")]) #comment
+                    print(x) # comment loc
+                    if last_comment ==x and last_comment!=0:
+                        G.add_edge(neutural_entities[i][j-1],neutural_entities[i][j]) 
+                    else:
+                        G.add_node(neutural_entities[i][j])
+
+
+                    last_comment = x 
+                    
+                indexxx += int(neutural_comments.iloc[x, neutural_comments.columns.get_loc("Size")])
+                #print(indexxx)
+
+            
+        df_comment_index+=5 #ikinci array finishleyende +5 olmaidi
+        if df_comment_index> df_size:
+                break
+    pos = nx.spring_layout(G)
+    st.session_state.graph_neu, st.session_state.neu_pos =G,pos
+
     st.session_state.positive_entities = positive_entities
     st.session_state.negative_entities = negative_entities 
+    st.session_state.neutural_entities  = neutural_entities
     update.text("👍 Done")
     vectorizer = TfidfVectorizer(stop_words='english')
     X = vectorizer.fit_transform(comments_df['Comment'])
@@ -408,10 +531,25 @@ def analyze():
     plt.title('Positive Entities')
     update.empty()
     st.success("Analysis completed! :ok_hand:")
-    
-with st.spinner("Analyzing... :mag:"):
-    if button_pressed:
-        analyze()
+
+
+
+
+
+
+
+if  len(st.session_state.videos)>0:
+    max_commen_result = st.number_input("Enter the maximum number of comments for each video:", min_value=50, max_value=500, step=1)
+
+    button_pressed = st.button("Start Analysis")
+    with st.spinner("Analyzing... :mag:"):
+        if button_pressed:
+            analyze()
+
+
+
+
+
 
     
         
@@ -424,6 +562,10 @@ if st.session_state.graph_neg:
     st.subheader("📌 Negative Entity Relations")
     graph_html = generate_network_graph(st.session_state.graph_neg , st.session_state.neg_pos)
     st.components.v1.html(graph_html, width=800, height=600)
+    df = sna(st.session_state.graph_neg)
+
+    with st.expander("SNA"):
+        st.dataframe(df)
 
 
 
@@ -432,7 +574,20 @@ if st.session_state.graph_pos:
     st.subheader("📌 Positive Entity Relations")
     graph_html = generate_network_graph(st.session_state.graph_pos , st.session_state.pos_pos)
     st.components.v1.html(graph_html, width=800, height=520)
+    df = sna(st.session_state.graph_pos)
+    
+    with st.expander("SNA"):
+        st.dataframe(df)
 
+
+if st.session_state.graph_neu:
+    st.subheader("📌 Neutural Entity Relations")
+    graph_html = generate_network_graph(st.session_state.graph_neu , st.session_state.neu_pos)
+    st.components.v1.html(graph_html, width=800, height=520)
+    df = sna(st.session_state.graph_neu)
+    
+    with st.expander("SNA"):
+        st.dataframe(df)
 
 
 
@@ -448,27 +603,96 @@ if st.session_state.wordcloud_image and len(st.session_state.wordcloud_image)>0:
 
     image_container.image(selected_image_path, caption='Selected Image')
 
-if len(st.session_state.positive_entities) > 0 and len(st.session_state.negative_entities) > 0:
-    neg_col, pos_col = st.columns(2)
-    neg_col.subheader("Entities in negative comments.")
-    pos_col.subheader("Entities in positive comments.")
-    neg_set = set()
-    pos_set = set()
+def make_clickable(entity):
+    return f'<a href="https://en.wikipedia.org/wiki/{entity}" target="_blank">{entity}</a>'
+
+
+if len(st.session_state.positive_entities) > 0 :
+    neg_list  = []
+    pos_list  = []
     for result in st.session_state.positive_entities:
         for link in result:
             title = link
             link = link.replace(" ", "_")
-            pos_set.add(link)
+
+            pos_list.append(link)
+
+    df = pd.DataFrame(pos_list, columns=['Entity'])
+    df['Frequency'] = df.groupby('Entity')['Entity'].transform('count')
+    df = df.drop_duplicates(subset='Entity')
+ 
+    df = df.sort_values(by='Frequency', ascending=False)
+    df = df.reset_index(drop=True)
+    df['Entity'] = df['Entity'].apply(make_clickable)
+
     
+
+if len(st.session_state.negative_entities) > 0:
+    neg_list  = []
     for result in st.session_state.negative_entities:
         for link in result:
             title = link
             link = link.replace(" ", "_")
-            neg_set.add(link)
-    for text in neg_set:
-        neg_col.markdown(f"[Wikipedia](https://en.wikipedia.org/wiki/{text}) {text}")
-    for text in pos_set:
-        pos_col.markdown(f"[Wikipedia](https://en.wikipedia.org/wiki/{text}) {text}")
+            neg_list.append(link)
+    neg_df = pd.DataFrame(neg_list, columns=['Entity'])
+    neg_df['Frequency'] = neg_df.groupby('Entity')['Entity'].transform('count')
+    neg_df = neg_df.drop_duplicates(subset='Entity')
+
+    neg_df = neg_df.sort_values(by='Frequency', ascending=False)
+    neg_df = neg_df.reset_index(drop=True)
+    neg_df['Entity'] = neg_df['Entity'].apply(make_clickable)
+
+
+if len(st.session_state.neutural_entities) > 0 :
+
+    
+
+    neu_list  = []
+
+    for result in st.session_state.neutural_entities:
+        for link in result:
+            title = link
+            link = link.replace(" ", "_")
+
+            neu_list.append(link)
+
+    neu_df = pd.DataFrame(neu_list, columns=['Entity'])
+    neu_df['Frequency'] = neu_df.groupby('Entity')['Entity'].transform('count')
+    neu_df = neu_df.drop_duplicates(subset='Entity')
+ 
+    neu_df = neu_df.sort_values(by='Frequency', ascending=False)
+    neu_df = neu_df.reset_index(drop=True)
+    neu_df['Entity'] = neu_df['Entity'].apply(make_clickable)
+# Display the DataFrame with clickable Wikipedia links
+
+
+
+with st.expander("Positive entites"):
+    st.write(df.to_html(escape=False, render_links=True), unsafe_allow_html=True)
+with st.expander("Negative entites"):
+    st.write(neg_df.to_html(escape=False, render_links=True), unsafe_allow_html=True)
+with st.expander("Neutral entites"):
+    st.write(neu_df.to_html(escape=False, render_links=True), unsafe_allow_html=True)
+with st.expander("All entites"):
+    all_list = []
+    all_ent = st.session_state.neutural_entities + st.session_state.negative_entities + st.session_state.positive_entities
+    for result in all_ent:
+        for link in result:
+            title = link
+            link = link.replace(" ", "_")
+
+            all_list.append(link)
+    all_df = pd.DataFrame(all_list, columns=['Entity'])
+    all_df['Frequency'] = all_df.groupby('Entity')['Entity'].transform('count')
+    all_df = all_df.drop_duplicates(subset='Entity')
+ 
+    all_df = all_df.sort_values(by='Frequency', ascending=False)
+    all_df = all_df.reset_index(drop=True)
+    all_df['Entity'] = all_df['Entity'].apply(make_clickable)
+
+    st.write(all_df.to_html(escape=False, render_links=True), unsafe_allow_html=True)
+
+
 
 
 
